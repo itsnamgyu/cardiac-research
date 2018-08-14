44ad3a7e804490fc1218b69cccb6611debe56c95
Namgyu Ho <itsnamgyu@gmail.com>
2018-08-14 18:06:03 +0900

    restore regression module (needed in labeler)

--- retrain/regression.py
+++ retrain/regression.py
@@ -14,21 +14,15 @@ def regress(values):
 
 
 def main():
-    regr = linear_model.LinearRegression()
     values = np.array([1, 4, 3, 8, 9, 13, 12, 15])
-    regr.fit(np.arange(len(values)).reshape(-1 ,1), values)
+    results = regress(values)
 
-    results = regr.predict(np.arange(len(values)).reshape(-1, 1))
-    print('Results: \n', results)
-
-    # Plot outputs
     plt.scatter(np.arange(len(values)), values,  color='black')
-    plt.plot(np.arange(len(values)), results,  color='blue')
-
-    plt.xticks(())
-    plt.yticks(())
+    plt.plot(results,  color='blue')
 
     plt.show()
+
+    print('MSE:', mean_squared_error(values, results))
 
 
 if __name__ == '__main__':

87d6ba97ee43b2a8a8c97e50b8aa7808cbbf929d
Namgyu Ho <itsnamgyu@gmail.com>
2018-08-21 16:51:18 +0900

    separate image export function + remove augment
    Remove augmentation since Keras supports online augmentation.
    
    Oversample images as a hotfix, since Keras doesn't support balancing.

--- slice_range/cr_interface.py
+++ slice_range/cr_interface.py
@@ -13,13 +13,13 @@ import imageio
 import matplotlib.pyplot as plt
 import pandas as pd
 
-module_dir = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DATABASE_DIR = os.path.join(module_dir, 'data/database')
-METADATA_FILE = os.path.join(module_dir, 'data/metadata.json')
-IMAGES_DIR = os.path.join(module_dir, 'images')
-RESULTS_DIR = os.path.join(module_dir, 'results')
-SPEC_CSV = os.path.join(module_dir, 'analysis/images_spec.csv')
+DATABASE_DIR = os.path.join(PROJECT_DIR, 'data/database')
+METADATA_FILE = os.path.join(PROJECT_DIR, 'data/metadata.json')
+IMAGES_DIR = os.path.join(PROJECT_DIR, 'images')
+RESULTS_DIR = os.path.join(PROJECT_DIR, 'results')
+SPEC_CSV = os.path.join(PROJECT_DIR, 'analysis/images_spec.csv')
 
 
 '''
@@ -95,189 +95,6 @@ def parse_cr_code(cr_code, match=True):
         raise Exception('could not parse cr code {}'.format(cr_code))
 
     return tuple(map(lambda index: int(index), match.groups()))
-
-
-def prepare_images(tri_label=False, n_rotation=6, n_oversample=1,
-                   datasets=[0], train_split=0.8,
-                   train_datasets=None, test_datasets=None,
-                   spec_csv=SPEC_CSV):
-    '''
-    Arguments
-    n_rotation: augmentation using rotation, linearly spaced from 0 deg to 90 deg inclusive
-    n_oversample: augmentation multiplier for oap, obs (balancing tri-label data)
-    datasets: list of dataset indices to use for train + eval
-    train_split: ratio of training images
-    train_datasets: list of dataset indices to use for training
-    test_datasets: list of dataset indices to use for testing
-
-    Note that train/test_datasets overrides datasets, train_split
-    '''
-
-    def cut_list(sorted_list, ratio):
-        '''
-        Get a tuple of two lists that each contain ratio, 1 - ratio of
-        the original list
-        '''
-        n1 = int(len(sorted_list) * ratio)
-        # n2 = len(sorted_list) - n1  # unused
-        first = sorted_list[:n1]
-        second = sorted_list[n1:]
-
-        return first, second
-
-    def save_augmented_images(src_path, dest_path, n_rotation, crop_ratio=0.2):
-        def append_to_basename(jpg_path, append_string):
-            if jpg_path[-4:] != '.jpg':
-                raise ValueError("jpg_path does not conform to format '*.jpg'")
-
-            return jpg_path[:-4] + append_string + '.jpg'
-
-        angles = np.linspace(0, 90, n_rotation, dtype=int)
-
-        if len(np.unique(angles)) != len(angles):
-            raise Exception('Too many augmentations')
-
-        loaded_image = plt.imread(src_path)
-
-        for angle in angles:
-            augmented_path = append_to_basename(
-                dest_path, '_CP{:02d}_R{:03d}.aug'.format(int(crop_ratio * 100), angle))
-
-            lx, ly = loaded_image.shape
-            cropped_image = loaded_image[int(lx * crop_ratio): int(- lx * crop_ratio),
-                                         int(ly * crop_ratio): int(- ly * crop_ratio)]
-
-            rotated_image = scipy.ndimage.interpolation.rotate(
-                cropped_image, angle)
-
-            imageio.imsave(augmented_path, rotated_image)
-
-    print('Saving images...')
-    metadata = load_metadata()
-
-    # train_split patients
-    if train_datasets is None and test_datasets is None:
-        used_datasets = datasets
-        pids = []
-        for cr_code in metadata:
-            cr = parse_cr_code(cr_code)
-            if cr[0] in datasets:
-                pids.append(cr[1])
-        pids = sorted(list(set(pids)))
-        train_pids, test_pids = cut_list(pids, train_split)
-    elif train_datasets is None or test_datasets is None:
-        raise Exception(
-            'train_datasets and test_datasets must be passed in conjuction')
-    else:
-        used_datasets = train_datasets + test_datasets
-        train_pids = []
-        test_pids = []
-        for cr_code in metadata:
-            cr = parse_cr_code(cr_code)
-            if cr[0] in train_datasets:
-                train_pids.append(cr[1])
-            if cr[0] in test_datasets:
-                test_pids.append(cr[1])
-        train_pids = sorted(list(set(train_pids)))
-        test_pids = sorted(list(set(test_pids)))
-        pids = train_pids + test_pids
-
-    print('Training Patients: {} / Testing Patients: {}'.format(len(train_pids), len(test_pids)))
-
-    # init dataset info DataFrame
-    if tri_label:
-        semi_columns = ['OAP', 'IN', 'OBS', 'TOTAL']
-    else:
-        semi_columns = ['OAP', 'AP', 'MD', 'BS', 'OBS', 'TOTAL']
-    columns = pd.MultiIndex.from_product([['Original', 'Augmented'], semi_columns])
-    train_df = pd.DataFrame(np.zeros((3, 8)),
-                      columns=columns,
-                      index=('Image Count', 'Percentages', 'Images Per Patient'))
-    test_columns = pd.Index(semi_columns)
-    test_df = pd.DataFrame(np.zeros((3, 4)),
-                      columns=test_columns,
-                      index=('Image Count', 'Percentages', 'Images Per Patient'))
-
-    # count total # of cr_codes to consider (for loading bar)
-    count = len([None for cr_code in metadata if parse_cr_code(
-        cr_code)[0] in used_datasets])
-    bar = progress.bar.IncrementalBar('Copying Images...', max=count)
-
-    # check IMAGES_DIR
-    if glob.glob(os.path.join(IMAGES_DIR, '**/*.jpg'), recursive=True):
-        print('WARNING: there are existing images in "{}"'.format(IMAGES_DIR))
-
-    # copy images
-    unlabeled = []
-    done = []
-    for cr_code, info in metadata.items():
-        cr = parse_cr_code(cr_code)
-        if cr[0] not in used_datasets:
-            continue
-        bar.next()
-        done.append(cr_code)
-
-        try:
-            label = info['label']
-        except KeyError:
-            unlabeled.append(cr_code)
-            label = 'in'  # hotfix: TODO fix cr_learn to accept non-labeled test data
-
-        if tri_label and label in ['ap', 'md', 'bs']:
-            label = 'in'
-
-        if cr[1] in train_pids:
-            dest = 'training_{}.jpg'.format(cr_code)
-        else:
-            dest = 'testing_{}.jpg'.format(cr_code)
-
-        dest = os.path.join(label, dest)
-        dest = os.path.join(IMAGES_DIR, dest)
-        src = os.path.join(DATABASE_DIR, cr_code + '.jpg')
-        os.makedirs(os.path.dirname(dest), exist_ok=True)
-
-        if cr[1] in train_pids:
-            if label == 'in':
-                count = n_rotation
-            else:
-                count = n_rotation * n_oversample
-            save_augmented_images(src, dest, count)
-            train_df[('Original', label.upper())] += 1
-            train_df[('Augmented', label.upper())] += count
-        else:
-            shutil.copy(src, dest)
-            test_df[label.upper()] += 1
-    bar.finish()
-
-    # dataset summary overview
-    total = train_df.loc['Image Count', 'Original'].sum()
-    for i in range(3):
-        train_df.loc['Percentages',columns[i]] = train_df.loc['Image Count', columns[i]] / total * 100
-    total = train_df.loc['Image Count', 'Augmented'].sum()
-    for i in range(4, 7):
-        train_df.loc['Percentages',columns[i]] = train_df.loc['Image Count', columns[i]] / total * 100
-    train_df.loc['Images Per Patient'] = train_df.loc['Image Count'] / len(train_pids)
-    for label in ['Original', 'Augmented']:
-        train_df.loc[:,(label, 'TOTAL')] = train_df.loc[:, label].sum(axis=1)
-
-    total = test_df.loc['Image Count'].sum()
-    test_df.loc['Percentages'] = test_df.loc['Image Count'] / total * 100
-    test_df.loc['Images Per Patient'] = test_df.loc['Image Count'] / len(test_pids)
-    test_df['TOTAL'] = test_df.sum(axis=1)
-
-    # save dataset info to csvs
-    train_csv = os.path.join(os.path.dirname(spec_csv), 'train_' + os.path.basename(spec_csv))
-    test_csv = os.path.join(os.path.dirname(spec_csv), 'test_' + os.path.basename(spec_csv))
-    train_df.to_csv(train_csv)
-    test_df.to_csv(test_csv)
-    print('saved train dataset info to' + train_csv)
-    print('saved test dataset info to' + test_csv)
-
-    # print copied images
-    print('Copied')
-    print(done)
-    print('Unlabeled')
-    print(unlabeled)
 
 
 def visualize_metadata():
@@ -382,14 +199,7 @@ def is_tri_label_result(result):
 
 
 def main():
-    # visualize_metadata()
-    # save_training_data()
-    # save_training_data()
-
-    #prepare_images(tri_label=True, n_rotation=6, n_oversample=1,
-    #               train_datasets=[0], test_datasets=[3])
-    prepare_images(tri_label=True, n_rotation=6, n_oversample=5,
-                   datasets=[0])
+    pass
 
 
 if __name__ == "__main__":

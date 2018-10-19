2dec8e2bba17559c6453d63cf2d4609c30d78c7e
Namgyu Ho <itsnamgyu@gmail.com>
2018-10-19 22:48:03 +0900

    add personal notification function

---
+++ slice_range/lib.py
@@ -0,0 +1,35 @@
+import time
+import requests
+
+class Timer(object):
+    def __init__(self, name=None):
+        self.name= name
+
+    def print_time(self, time):
+        if self.name:
+            print('Elapsed Time [{}]: {:,.3f}s'.format(self.name, time))
+        else:
+            print('Elapsed Time: {:,.3f}s'.format(time))
+
+    def measure(self, f, repeat=1):
+        start = time.time()
+        for _ in range(repeat):
+            f()
+        elapsed = time.time() - start
+        self.print_time(elapsed)
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        elapsed = time.time() - self.start
+        self.print_time(elapsed)
+
+
+def notify(string='done'):
+    # send a personal notification to Namgyu
+    headers = {
+        'Content-type': 'application/json',
+    }
+    data = '{"text":"[CREB1] %s"}' % string
+    response = requests.post('https://hooks.slack.com/services/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', headers=headers, data=data)

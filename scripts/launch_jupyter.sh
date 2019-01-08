764ac2331e66c734ceca6062c8ebb7088502eb15
Namgyu Ho <itsnamgyu@gmail.com>
2019-01-08 16:11:15 +0900

    Change jupyter session name
    And auto-kill session on restart.

--- scripts/launch_jupyter.sh
+++ scripts/launch_jupyter.sh
@@ -1,3 +1,4 @@
 # deploy script for Jupyter
 source venv/bin/activate
-tmux new -s jup -d 'jupyter notebook --ip=0.0.0.0 --port=8010 2>&1 | tee jupyter.log'
+tmux kill-session -t cr_jup
+tmux new -s cr_jup -d 'jupyter notebook --ip=0.0.0.0 --port=8010 2>&1 | tee jupyter.log'

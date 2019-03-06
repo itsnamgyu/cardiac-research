196edd387d92f166f2d724b3c52dad4ff8c9a616
Namgyu Ho <itsnamgyu@gmail.com>
2019-03-06 00:37:59 -0500

    Minor docstring,logging changes

--- lib.py
+++ lib.py
@@ -3,6 +3,11 @@ import requests
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 class Timer(object):
+    """
+    Usage:
+        with Timer(name='Title') as t:
+            do_something()
+    """
     def __init__(self, name=None):
         self.name= name
 
@@ -32,7 +37,7 @@ def notify(string='done'):
     headers = {
         'Content-type': 'application/json',
     }
-    data = '{"text":"[CREB1] %s"}' % string
+    data = '{"text":"[CR] %s"}' % string
     response = requests.post('https://hooks.slack.com/services/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', headers=headers, data=data)
 
 def onehot(labels):

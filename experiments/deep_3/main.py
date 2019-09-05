ce3c66687667ae773d167e7acbb7e95554179ef3
Namgyu Ho <itsnamgyu@gmail.com>
2019-09-05 13:28:21 -0700

    Update experiment code with baselinemodel

--- experiments/deep_3/main.py
+++ experiments/deep_3/main.py
@@ -41,15 +41,16 @@ LEARNING_RATES = [0.01, 0.001, 0.0001]
 EPOCHS = 50
 # experiment index to track saved model weights, training history etc.
 # iterate this index for each run (make sure to keep track of this index)
-EXP = 7
+EXP = 101
 # whether to sample 10% of all slices (for sanity checking purposes)
 SAMPLE = False
 # seed for k-fold split
 K_SPLIT_SEED = 1
 # models to train
 MODEL_KEYS = [
+    'baselinemodelv1'
     #'xception',
-    'mobileneta25',
+    #'mobileneta25',
     #'mobilenetv2a35',
     #'vgg16',
     #'resnet50v2',
